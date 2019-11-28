2b7b915a6a2271daf110cd168d4995286405b26e
frank <847923458@qq.com>
2019-11-28 13:21:33 +0800

    update

--- arm_nnexamples_cifar10/ARMCM7_debug/retarget_io.d
+++
@@ -1,8 +0,0 @@
-.\armcm7_debug\retarget_io.o: C:\Users\Administrator\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.6.2\Source\retarget_io.c
-.\armcm7_debug\retarget_io.o: C:\MyPlace\Software\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\armcm7_debug\retarget_io.o: C:\MyPlace\Software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\armcm7_debug\retarget_io.o: C:\MyPlace\Software\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\armcm7_debug\retarget_io.o: C:\MyPlace\Software\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\armcm7_debug\retarget_io.o: C:\MyPlace\Software\Keil_v5\ARM\ARMCC\Bin\..\include\assert.h
-.\armcm7_debug\retarget_io.o: C:\MyPlace\Software\Keil_v5\ARM\ARMCC\Bin\..\include\rt_sys.h
-.\armcm7_debug\retarget_io.o: .\RTE\_ARMCM7_SP\RTE_Components.h
